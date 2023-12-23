49f40a3ed1bf5193f9122a8389777c68bf5c9e98
Alex11392 <125459456+Alex11392@users.noreply.github.com>
2023-12-23 12:36:29 +0800

    Update Customer.java
    新增getData()

--- Java_by_Alex/Customer.java
+++ Java_by_Alex/Customer.java
@@ -2,6 +2,8 @@ package app;
 import java.time.LocalDate;
 import java.util.Date;
 
+import org.json.JSONObject;
+
 public class Customer extends Member{
 	private int customer_id;
 	private int customer_point;
@@ -26,5 +28,15 @@ public class Customer extends Member{
 	}
 	public int getCustomer_point(){
 		return customer_point;
+	}
+
+	public JSONObject getData(){
+		JSONObject jso = super.getData(); // 使用父類別的 getData() 取得基本資料
+
+        // 加入額外的資料
+        jso.put("customer_id", getCustomer_id());
+        jso.put("customer_point", getCustomer_point());
+
+        return jso;
 	}
 }
