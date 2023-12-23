096d6da24b415ab127aaa4ea013684b665f0308b
Alex11392 <125459456+Alex11392@users.noreply.github.com>
2023-12-23 12:37:27 +0800

    Update HotelOwner.java
    新增hotel_id
    新增getData()

--- Java_by_Alex/HotelOwner.java
+++ Java_by_Alex/HotelOwner.java
@@ -2,18 +2,37 @@ package app;
 import java.time.LocalDate;
 import java.util.Date;
 
+import org.json.JSONObject;
+
 public class HotelOwner extends Member{
 	private int hotelowner_id;
+	private int hotel_id;
+
 	//register
-	public HotelOwner(String name,String email,String password,LocalDate birthday,String intro){
+	public HotelOwner(String name, String email, String password, LocalDate birthday, String intro){
 		super(name,email,password,birthday,intro);
 	}
 	//revise,getData
-	public HotelOwner(int hotelowner_id,String name,String email,String password,LocalDate birthday,String intro,Date modified_time) {
+	public HotelOwner(int hotelowner_id, String name, String email, String password, LocalDate birthday, String intro, Date modified_time, int hotel_id) {
 		super(name,email,password,birthday,intro,modified_time);
 		this.hotelowner_id = hotelowner_id;
+		this.hotel_id = hotel_id;
 	}
 	public int getHotelOwnerId(){
 		return hotelowner_id;
+	}
+
+	public int getHotelId(){
+		return hotel_id;
+	}
+
+	public JSONObject getData(){
+		JSONObject jso = super.getData(); // 使用父類別的 getData() 取得基本資料
+
+        // 加入額外的資料
+        jso.put("hotelowner_id", getHotelOwnerId());
+        jso.put("hotel_id", getHotelId());
+
+        return jso;
 	}
 }
