71f9041b56fcb3301a0f568e9ca72c68b0c953f3
Alex11392 <125459456+Alex11392@users.noreply.github.com>
2023-12-24 19:39:05 +0800

    Add files via upload

---
+++ Java_by_Alex/Hotel.java
@@ -0,0 +1,58 @@
+package app;
+
+import org.json.JSONObject;
+
+public class Hotel {
+	private int hotel_id;
+	private String hotelname;
+	private String location;
+	private String image;
+	private String facilities;
+	private String intro;
+	
+	//add
+	public Hotel(String hotelname,String location,String image,String facilities,String intro){
+		this.hotelname = hotelname;
+		this.location = location;
+		this.image = image;
+		this.facilities = facilities;
+		this.intro = intro;
+	}
+	//revise
+	public Hotel(int hotel_id,String hotelname,String location,String image,String facilities,String intro) {
+		this.hotel_id = hotel_id;
+		this.hotelname = hotelname;
+		this.location = location;
+		this.image = image;
+		this.facilities = facilities;
+		this.intro = intro;
+	}
+	
+	public int getHotel_id(){
+		return hotel_id;
+	}
+	public String getHotelName() {
+		return hotelname;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public String getImage(){
+		return image;
+	}
+	public String getFacilities() {
+		return facilities;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public JSONObject getData(){
+		JSONObject jso = new JSONObject();
+		jso.put("hotelname",getHotelName());
+		jso.put("location",getLocation());
+		jso.put("image",getImage());
+		jso.put("facilities",getFacilities());
+		jso.put("introduction",getIntro());
+		return jso;
+	}
+}
