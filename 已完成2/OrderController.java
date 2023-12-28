e3b7c1053618edfaa825959f08db41aade95d5b8
Dowgojashan <134786599+Dowgojashan@users.noreply.github.com>
2023-12-29 03:38:34 +0800

    Update OrderController.java

--- 已完成2/OrderController.java
+++ 已完成2/OrderController.java
@@ -34,36 +34,46 @@ public class OrderController extends HttpServlet {
         	int order_number = jso.getInt("order_number"); 
         	int order_price = jso.getInt("order_price");
         	int number_of_guest = jso.getInt("number_of_guest");
+        	String creditcard = jso.getString("creditcard");
+        	String email = jso.getString("email");
         	
-        	String sbooking_date = jso.getString("booking_date");
+        	
         	String scheckin_date = jso.getString("checkin_date");
         	String scheckout_date = jso.getString("checkout_date");
         	// 定義日期字符串的格式
     		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
     		// 將字符串轉換為 LocalDate
-        	LocalDate booking_date = LocalDate.parse(sbooking_date, formatter);
         	LocalDate checkin_date = LocalDate.parse(scheckin_date, formatter);
         	LocalDate checkout_date = LocalDate.parse(scheckout_date, formatter);
-        	String email = jso.getString("email");
-            
-        	String customer_point_use = jso.getString("customer_point_use");
+        	
+        	//String customer_point_use = jso.getString("customer_point_use");
         	boolean cpu = false;
-        	if(customer_point_use.equals("true")) {cpu = true;}
-            /** 建立一個新的Room物件 */
-            Order o = new Order(room_id, customer_id, order_number, order_price, number_of_guest, booking_date, checkin_date, checkout_date,email);
-            
-            /** 透過RoomHelper物件的create()方法新建一個房型至資料庫 */
-            JSONObject data = oh.checkout(o, cpu);
+        	//if(customer_point_use.equals("true")) {cpu = true;}
+        	
+            if(oh.check_credit_card(creditcard) == false) {
+            	/** 以字串組出JSON格式之資料 */
+                String resp = "{\"status\": \'400\', \"message\": \'結帳失敗，沒有此張信用卡！\', \'response\': \'\'}";
+                /** 透過JsonReader物件回傳到前端（以字串方式） */
+                jsr.response(resp, response);
+            	/** 建立一個新的Room物件 */
                 
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("message", "成功! 新增房型資料...");
-            resp.put("response", data);
+            }
+            else {
+            	Order o = new Order(room_id, customer_id, order_number, order_price, number_of_guest, checkin_date, checkout_date,email);
                 
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
-            
+                /** 透過RoomHelper物件的create()方法新建一個房型至資料庫 */
+                JSONObject data = oh.checkout(o, cpu);
+                oh.deleteByID(customer_id, room_id);
+                    
+                /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                JSONObject resp = new JSONObject();
+                resp.put("status", "200");
+                resp.put("message", "成功! 結帳完成且購物車刪除成功...");
+                resp.put("response", data);
+                    
+                /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                jsr.response(resp, response);
+            }
         }
 
     /**
@@ -108,15 +118,16 @@ public class OrderController extends HttpServlet {
             JSONObject jso = jsr.getObject();
          
             /** 取出經解析到JSONObject之Request參數 */
-            int id = jso.getInt("order_id");
+            int customer_id = jso.getInt("customer_id");
+            int room_id = jso.getInt("room_id");
          
             /** 透過RoomHelper物件的deleteByID()方法至資料庫刪除該房型，回傳之資料為JSONObject物件 */
-            JSONObject query = oh.deleteByID(id);
+            JSONObject query = oh.deleteByID(customer_id,room_id);
          
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
             JSONObject resp = new JSONObject();
             resp.put("status", "200");
-            resp.put("message", "訂單刪除成功！");
+            resp.put("message", "從購物車成功！");
             resp.put("response", query);
 
             /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
